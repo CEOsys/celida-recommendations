47cc1779cbcecc1480612945067fa29e95314f26
Gregor Lichtner <gregor.lichtner@charite.de>
2023-06-02 11:09:47 +0200

    reset ig publisher to latest version from HL7

--- scripts/run-ig-publisher.sh
+++ scripts/run-ig-publisher.sh
@@ -8,5 +8,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 sushi .
 
 # download & run publisher
-curl -L https://github.com/CEOsys/fhir-ig-publisher/releases/latest/download/publisher.jar -o ./input-cache/publisher.jar --create-dirs
+curl -L https://github.com/HL7/fhir-ig-publisher/releases/latest/download/publisher.jar -o ./input-cache/publisher.jar --create-dirs
 java -jar input-cache/publisher.jar publisher -ig . -no-sushi
